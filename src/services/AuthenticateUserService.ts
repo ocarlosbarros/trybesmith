f8ffc1c9469d8e0c6e13aa6a7061c74ecb4e6181
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-31 15:50:06 -0300

    feat:creates AuthenticateUserService

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,33 @@
+import Jwt from 'jsonwebtoken';
+import IUser from '../intefaces/IUser';
+import connection from '../models/connection';
+import UserModel from '../models/UserModel';
+
+export default class AuthenticateUserService {
+  public model: UserModel;
+  
+  private secret = process.env.JWT_SECRET || '';
+
+  getSecret() {
+    return this.secret;
+  }
+
+  constructor() {
+    this.model = new UserModel(connection);
+  }
+
+  public authenticate = async (user:IUser) => {
+    const { id } = user;
+    
+    if (!id) return;
+    
+    const founded = await this.model.findById(id);
+
+    if (!founded) throw new Error('Invalid fields');
+
+    const secret = this.getSecret(); 
+    const token = Jwt.sign({ payload: founded.id }, secret);
+
+    return token;
+  };
+}

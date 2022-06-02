78ef14e733684771548c5ee1703410d9c8336b8a
Carlos Barros <carlos_dbs@hotmail.com>
2022-06-02 15:12:34 -0300

    feat:creates create method in OrderModel

--- src/models/OrderModel.ts
+++ src/models/OrderModel.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import IOrder from '../intefaces/IOrder';
 
 export default class OrderModel {
@@ -14,11 +14,19 @@ export default class OrderModel {
     FROM Trybesmith.Orders AS O
       INNER JOIN Trybesmith.Products AS P ON P.orderId = O.id
       GROUP BY O.userId, O.id;
-    
-
-
     `;
     const [orders] = await this.connection.execute(query);
     return orders as IOrder[];
+  };
+
+  public create = async (userId: number): Promise<number> => {
+    const query = 'INSERT INTO Trybesmith.Orders(userId) VALUES (?);';
+    const result = await this.connection.execute<ResultSetHeader>(
+      query,
+      [userId],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return insertId;
   };
 }

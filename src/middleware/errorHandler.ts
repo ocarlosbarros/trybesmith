d4098e0137c215130f0ec118260b47ec9ab510c7
Carlos Barros <carlos_dbs@hotmail.com>
2022-06-02 13:54:01 -0300

    feat:add JsonWebTokenError to errorhandler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -4,7 +4,7 @@ import Joi from 'joi';
 
 type CustomError = {
   name:string,
-  details?: Array<Joi.AnySchema>,
+  details: Array<Joi.AnySchema>,
   message?: string,
 };
 
@@ -12,12 +12,14 @@ const handler = async (error: Error, request: Request, response: Response, next:
   const { name, details, message } = error as CustomError;
   switch (name) {
     case 'ValidationError': {
-      if (!details) break;
       const statusCode: number = details[0].type === 'any.required' ? 400 : 422; 
       return response.status(statusCode).json({ message: details[0].message });
     }
     case 'AuthenticationError': 
       return response.status(401).json({ message });
+
+    case 'JsonWebTokenError': 
+      return response.status(401).json({ message: 'Invalid token' });
     default:
       console.error(error);
       response.sendStatus(500);

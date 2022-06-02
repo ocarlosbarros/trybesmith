bb7f6cc5b2d2d68f1c12221d860ced2a843b9454
Carlos Barros <carlos_dbs@hotmail.com>
2022-06-02 15:12:06 -0300

    feat:adds post route to orders.

--- src/routes/OrderRouter.ts
+++ src/routes/OrderRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrderController from '../controllers/OrderController';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import validateOrder from '../middleware/validateOrder';
 
 const OrderRouter = Router();
 
 const orderController = new OrderController();
 
 OrderRouter.get('/', orderController.findAll);
+OrderRouter.post('/', ensureAuthenticated, validateOrder, orderController.create);
 
 export default OrderRouter;
